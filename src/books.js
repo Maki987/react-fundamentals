642e79d265f9dd9a75dbc62c32ea84f631fb3236
Maki987 <makiii9089@gmail.com>
2023-06-28 15:16:59 +0200

    Added title and number of book

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+
+export const books = [
+  {
+    author: 'Alex Michaelides',
+    title: 'The Fury',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'Lucy Score',
+    title: 'Things We Never Got Over',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Lucy Score',
+    title: 'The Covenant of Water',
+    img: img3,
+    id: 3,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -2,111 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { books } from './books';
+import Book from './Book';
 import reportWebVitals from './reportWebVitals';
-
-const books = [
-  {
-    author: 'Alex Michaelides',
-    title: 'The Fury',
-    img: './images/boook-1.jpg',
-    id: 1,
-  },
-  {
-    author: 'Lucy Score',
-    title: 'Things We Never Got Over',
-    img: 'https://m.media-amazon.com/images/I/419Q1FpTrzL._SX311_BO1,204,203,200_.jpg',
-    id: 2,
-  },
-];
 
 const img = './images/book-1.jpg';
 const title = 'The Fury';
 const author = 'Alex Michaelides';
 
 function BookList() {
-  const getBook = (id) => {
-    books.find((book) => book.id === id);
-    console.log(id);
-  };
   return (
-    <section className="booklist">
-      <EventExamples />
-      {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />;
-      })}
-    </section>
+    <>
+      <h1>amazon best seller</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
   );
 }
-
-const Book = (props) => {
-  console.log(props);
-  const { img, title, author, children, getBook, id } = props;
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4> {children}
-      <button onClick={() => getBook(id)}>click me</button>
-    </article>
-  );
-};
-
-const EventExamples = () => {
-  const handleFormInput = (e) => {
-    console.log(e.target);
-    console.log(e.target.name);
-    console.log(e.target.value);
-    console.log('handle form input');
-  };
-  const handleButtonClick = () => {
-    alert('handle form input');
-  };
-  const handleFormSubmission = (e) => {
-    e.preventDefault();
-    console.log('form submitted');
-  };
-  return (
-    <section>
-      <form onSubmit={handleFormSubmission}>
-        <h2>Typical Form</h2>
-        <input type="text" name="product" onChange={handleFormInput} style={{ margin: '1rem 0' }} />
-        <div>
-          <button onClick={() => console.log('click me')} type="submit">
-            submit
-          </button>
-        </div>
-      </form>
-    </section>
-  );
-};
-
-const Image = () => <img src="./images/book-1.jpg" alt="The Fury" />;
-const Title = () => {
-  return <h2>Alex Michaelides </h2>;
-};
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: '#617d98',
-    fontSize: '0.75rem',
-    marginTop: '0.5rem',
-  };
-  return <h4 style={inlineHeadingStyles}>Jordan Moore</h4>;
-};
-
-const people = [
-  {
-    name: 'peter',
-    job: 'developer',
-    age: 24,
-  },
-  { name: 'ann', job: 'designer', age: 25 },
-  { name: 'bob', job: 'tester', age: 35 },
-];
-
-//const joungPeople = people.filter((person) => person.age < 35);
-/*find - return single instance, returns first match */
-const joungPeople = people.find((person) => person.age < 35);
-console.log(joungPeople);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
